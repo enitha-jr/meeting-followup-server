0389c89108ae2d650d9161b1208f58fb3f718749
enitha-jr <enitharajeshkumar@gmail.com>
2024-12-04 09:43:07 +0530

    meetask

--- server.js
+++ server.js
@@ -67,6 +67,20 @@ app.get('/meetings/:meetingid/minutes', (req, res) => {
     });
 });
 
+app.get('/meetings/:meetingid/tasks', (req, res) => {
+    const sql = "SELECT * FROM tasks WHERE meetingid = ?";
+    const values = [req.params.meetingid];
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+
 app.post('/newmeeting', (req, res) => {
     const sql = "insert into meetings (followup,title,mid,dept,host,date,time,venue,description,members) values (?,?,?,?,?,?,?,?,?,?)";
     const values = [req.body.followup, req.body.title, req.body.mid, req.body.dept, req.body.host, req.body.date, req.body.time, req.body.venue, req.body.desc, req.body.members];
@@ -81,8 +95,10 @@ app.post('/newmeeting', (req, res) => {
 });
 
 app.post('/meetings/:meetingid/minutes', (req, res) => {
+    const { meetingid } = req.params;
+    const { minute } = req.body;
     const sql = "insert into minutes (meetingid,minute) values (?,?)";
-    const values = [req.body.meetingid, req.body.minute];
+    const values = [meetingid,minute];
     db.query(sql, values, (err, result) => {
         if (err) {
             console.log(err.message);
@@ -90,6 +106,33 @@ app.post('/meetings/:meetingid/minutes', (req, res) => {
             console.log(result);
             res.send(result);
         }
+    });
+});
+
+app.post('/meetings/:meetingid/tasks', (req, res) => {
+    const {meetingid} = req.params;
+    const { minute, task, desc, assign, date } = req.body;
+    const selectQuery = "select minuteid from minutes where minute = ? and meetingid = ?";
+    const insertQuery = "insert into tasks (meetingid,minuteid,task,description,assigned,date) values (?,?,?,?,?,?)";
+
+    db.query(selectQuery, [minute, meetingid], (err, result) => {
+        if(err){
+            console.log(err.message);
+        }
+        if (result.length === 0) {
+            res.status(404).send("Minute not found for the given meeting");
+            return;
+        }
+        const minuteid = result[0].minuteid;
+        const values = [meetingid, minuteid, task, desc, assign, date];
+        db.query(insertQuery, values, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result);
+                res.send(result); 
+            }
+        });
     });
 });
 
