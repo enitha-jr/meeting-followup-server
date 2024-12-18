d139158c6efc73ffce500395328e11491f4c72ae
Dharnesh10 <dharneshk10@gmail.com>
2024-12-18 08:52:43 +0530

    changes made

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'enithaJR',
-    database: 'meeting'
+    password: 'dharnesh10',
+    database: 'meetminutes'
 });
 
 db.connect((err) => {
